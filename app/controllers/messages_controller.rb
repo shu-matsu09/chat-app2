c2bed6fe2bdabe1d0a60745b9dbd2aaa23edcb53
shu-matsu09 <shusukem1988@gmail.com>
2021-01-21 14:41:55 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,15 +4,20 @@ class MessagesController < ApplicationController
     # パスにroom_idが含まれているため、paramsというハッシュオブジェクトの中に、room_idという値が存在している
     @room = Room.find(params[:room_id])
     @message = Message.new
+    # チャットルームに紐付いている全てのメッセージを@messageと定義。メッセージに紐付くユーザー情報の取得に、
+    # メッセージの数と同じ回数のアクセスが必要なため、includes(:user)とすることでN+1問題を解消
+    @messages = @room.messages.includes(:room)
   end
 
   def create
     @room = Room.find(params[:room_id])
-    # @room.messages.newでチャットルームに紐づいたメッセージのインスタンスを生成し、message_paramsを引数にして、privateメソッドを呼び出す
+    # @room.messages.newでチャットルームに紐づいたメッセージのインスタンス(messagesテーブルだからmessages)を生成し、message_paramsを引数にして、privateメソッドを呼び出す
     @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      # 投稿に失敗したときにindex.html.erbを参照するが、renderではindexアクションを経由しないため、@messagesが定義されていないとエラーになる。そのため、indexアクションと同様に@messageを定義する必要あり
+      @messages = @room.messages.includes(:room)
       render :index
     end
   end
